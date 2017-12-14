e16f37fde570ca40f5a3a423297b0886d3b487c7
apocalypse575 <apocalypse575@gmail.com>
2017-12-14 13:01:23 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  if array.length == 2
+    array.join(" and ")
+  elsif array.length >= 3
+    l = array.length
+    short = array.length - 2
+      array[0..short].join(", ") + ", and ".concat(array[l - 1])
+  else
+  return array.join()
+end
 end
