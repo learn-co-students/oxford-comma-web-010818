c3589fd238380fd839ab272ecd80d751d93a9d81
Katy Carr <khcarr9@gmail.com>
2017-12-17 14:50:53 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
 def oxford_comma(array)
-
+  length = array.length
+  if length == 1
+    array[0]
+  elsif length == 2
+    array[0]+" and "+array[1]
+  else
+    last = array.last
+    array.pop
+    string = array.join(", ")
+    string << ", and #{last}"
+    string
+  end
 end
